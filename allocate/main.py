0c8a4dd210d285a6586b4308447f71aab9fe52f6
jeffery71 <jefferynthu@gmail.com>
2020-10-23 17:37:22 +0800

    fix

--- allocate/main.py
+++ allocate/main.py
@@ -312,15 +312,15 @@ class NFVOPlugin(AllocateNSSIabc):
             'vnffgInfo': vnffg_info,
             'nsState': ns_state,
             'monitoringParameter': monitoringParameter,
-            'mongodb':res_show_ns['ns']['vnf_ids']['VNF0']
-            'nrfd':res_show_ns['ns']['vnf_ids']['VNF1']
-            'amfd':res_show_ns['ns']['vnf_ids']['VNF2']
-            'smfd':res_show_ns['ns']['vnf_ids']['VNF3']
-            'udrd':res_show_ns['ns']['vnf_ids']['VNF4']
-            'pcfd':res_show_ns['ns']['vnf_ids']['VNF5']
-            'udmd':res_show_ns['ns']['vnf_ids']['VNF6']
-            'nssfd':res_show_ns['ns']['vnf_ids']['VNF7']
-            'ausd':res_show_ns['ns']['vnf_ids']['VNF8']
+            'mongodb':res_show_ns['ns']['vnf_ids']['VNF0'],
+            'nrfd':res_show_ns['ns']['vnf_ids']['VNF1'],
+            'amfd':res_show_ns['ns']['vnf_ids']['VNF2'],
+            'smfd':res_show_ns['ns']['vnf_ids']['VNF3'],
+            'udrd':res_show_ns['ns']['vnf_ids']['VNF4'],
+            'pcfd':res_show_ns['ns']['vnf_ids']['VNF5'],
+            'udmd':res_show_ns['ns']['vnf_ids']['VNF6'],
+            'nssfd':res_show_ns['ns']['vnf_ids']['VNF7'],
+            'ausd':res_show_ns['ns']['vnf_ids']['VNF8'],
         }
 
     def list_vnf(self):
